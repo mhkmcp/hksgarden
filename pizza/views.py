dd4a48176ad9f8433780bd8f191e031d2bc43582
mhkmcp <mhkmcp@yahoo.com>
2021-03-24 02:23:23 +0600

    Added Model & Form

--- pizza/views.py
+++ pizza/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import PizzaForm, PizzaModelForm
 
 
 def home(request):
@@ -7,5 +8,25 @@ def home(request):
 
 
 def order(request):
-    context = {}
+    if request.method == 'POST':
+        filled_form = PizzaModelForm(request.POST)
+        if filled_form.is_valid():
+            note = 'Thanks for Ordering! Your %s %s and %s Pizza is on its way :) ' %(
+                filled_form.cleaned_data.get('size'),
+                filled_form.cleaned_data.get('topping1'),
+                filled_form.cleaned_data.get('topping2')
+            )
+            pizza_form = PizzaModelForm()
+            context = {
+                'pizza_form': pizza_form,
+                'note': note
+            }
+            # return render(request, 'pizza/order.html', context)
+
+    else:
+        pizza_form = PizzaModelForm()
+        context = {
+            'pizza_form': pizza_form
+        }
     return render(request, 'pizza/order.html', context)
+
